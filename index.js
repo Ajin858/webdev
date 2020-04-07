2e19f148508a82e1d7b1e63bcdccb388eb425ebe
Abhinav Jain <abhinavjn01@gmail.com>
2020-04-08 03:13:07 +0530

    Added billing and client side app

--- index.js
+++ index.js
@@ -25,15 +25,25 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+   
+    const path = require("path");
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
+
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'));
 
 
-    const path = require('path')
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'))
-    })
-}
+//     const path = require('path');
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'));
+//     });
+// }
 
 // app.get('/',(req , res)=>{
 //     res.send({ hi: 'there'});
