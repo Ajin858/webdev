cb9af05eb7a6de019691c38a3c15460ee6855c88
Abhinav Jain <abhinavjn01@gmail.com>
2020-04-08 23:52:19 +0530

    Added billing and client side app

---
+++ client/build/precache-manifest.b450e0f328f4df79dff411319de0ab68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9af9e8f9375af432c7beb65f5061814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe13650abc1130eed634",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "fe13650abc1130eed634",
+    "url": "/static/js/2.0fca085c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0fca085c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af87e8a92fae3d39d096",
+    "url": "/static/js/main.2b37e517.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
